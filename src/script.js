2490c7c0b8e30f2905c9c68eea5835893896f1ff
julaineb <julia.bula.net@gmail.com>
2018-09-09 15:17:09 +0200

    mounting srs cript js, adding scroll-to-top in js

--- src/script.js
+++ src/script.js
@@ -8,3 +8,24 @@
 
 //adding element manually - because IE
 document.createElement("main");
+
+//smooth scrolling
+let toTop = document.getElementById("to-top"),
+    scrollTimer,
+    scrollValue;
+
+let scrollToTop = function(event) {
+  event.preventDefault();
+  scrollValue = window.scrollY;
+  scrollTimer = setInterval(scrollStep, 10);
+};
+
+let scrollStep = function() {
+  scrollValue -= 30;
+  window.scrollTo(0, scrollValue);
+  if (scrollValue <= 0) {
+    clearInterval(scrollTimer);
+  };
+};
+
+toTop.addEventListener("click", scrollToTop);
