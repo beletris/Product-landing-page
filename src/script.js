493293262eedabc25be294d1dd5fbb7cce6b6809
julaineb <julia.bula.net@gmail.com>
2018-09-29 19:45:18 +0200

    better smooth scrolling, still does not work on IE

--- src/script.js
+++ src/script.js
@@ -1,32 +1,32 @@
-/*TO DO
-- navigation bar position fixed
- */
+//TO do {
+// - IE, get a hold of yourself;
 
 //adding element manually - because IE
 document.createElement("main");
 
-
 //navbar fixed
 const navBar = document.getElementById("nav-bar");
+const headBar = document.getElementById("header");
 
 window.onscroll = function scrollToStick() {
-  if (window.pageYOffset > 530) {  // height of header - height of navbar
+  if (window.pageYOffset > (headBar.clientHeight - navBar.clientHeight)) {  // height of header - height of navbar
     navBar.classList.add("sticky-dark");
   } else {
     navBar.classList.remove("sticky-dark");
   };
 };
 
+
+//for scrolling IE does not work
 //scroll to top
 const toTop = document.getElementById("to-top");
-let scrollTimer,
-    scrollValue;
 
 const scrollToTop = function(event) {
   event.preventDefault();
   scrollValue = window.scrollY;
   scrollTimer = setInterval(scrollStep, 10);
 };
+
 const scrollStep = function() {
   scrollValue -= 30;
   window.scrollTo(0, scrollValue);
